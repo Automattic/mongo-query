aff72323bd5274c6fb3208d3eb284b0af031b0ec
Guillermo Rauch <rauchg@gmail.com>
2012-09-19 16:06:28 -0700

    test: tweaks and fixed cross-browser compat

--- test/filter.operators.js
+++ test/filter.operators.js
@@ -6,37 +6,37 @@ var filter = require('../filter');
 var operators = filter.operators;
 var expect = require('expect.js');
 
-describe('expect(operators', function(){
+describe('filter operators', function(){
 
-  it('$gt).to work', function () {
+  it('$gt should work', function () {
     expect(operators.$gt(1,0)).to.be(true);
     expect(operators.$gt(0,1)).to.be(false);
   });
 
-  it('$gte).to work', function () {
+  it('$gte should work', function () {
     expect(operators.$gte(1,0)).to.be(true);
     expect(operators.$gte(1,1)).to.be(true);
     expect(operators.$gte(0,1)).to.be(false);
   });
 
-  it('$lt).to work', function () {
+  it('$lt should work', function () {
     expect(operators.$lt(0,1)).to.be(true);
     expect(operators.$lt(1,0)).to.be(false);
   });
 
-  it('$lte).to work', function () {
+  it('$lte should work', function () {
     expect(operators.$lte(0,1)).to.be(true);
     expect(operators.$lte(1,1)).to.be(true);
     expect(operators.$lte(1,0)).to.be(false);
   });
 
-  it('$all).to work', function () {
+  it('$all should work', function () {
     expect(operators.$all([1,2],[1,2])).to.be(true);
     expect(operators.$all([1], [1,2])).to.be(false);
     expect(operators.$all([1,2,3],[1,2])).to.be(true);
   });
 
-  it('$exists).to work', function () {
+  it('$exists should work', function () {
     var a;
     var b = {c: 'hi'};
     expect(operators.$exists(a, false)).to.be(true);
@@ -47,35 +47,35 @@ describe('expect(operators', function(){
     expect(operators.$exists('hi', true)).to.be(true);
   });
 
-  it('$mod).to work', function () {
+  it('$mod should work', function () {
     expect(operators.$mod(12, [12, 0])).to.be(true);
     expect(operators.$mod(24, [12, 0])).to.be(true);
     expect(operators.$mod(15, [12, 0])).to.be(false);
   });
 
-  it('$ne).to work', function () {
+  it('$ne should work', function () {
     expect(operators.$ne(12,12)).to.be(false);
     expect(operators.$ne(12,11)).to.be(true);
   });
 
-  it('$in).to work', function () {
+  it('$in should work', function () {
     expect(operators.$in(1,[0,1,2])).to.be(true);
     expect(operators.$in(4,[0,1,2])).to.be(false);
   });
 
-  it('$nin).to work', function () {
+  it('$nin should work', function () {
     expect(operators.$nin(1,[0,1,2])).to.be(false);
     expect(operators.$nin(4,[0,1,2])).to.be(true);
   });
 
-  it('$size).to work', function () {
+  it('$size should work', function () {
     expect(operators.$size([0,1,2], 3)).to.be(true);
     expect(operators.$size('foo', 3)).to.be(true);
     expect(operators.$size({ a: 1}, 1)).to.be(false);
     expect(operators.$size({ length: 3}, 3)).to.be(true);
   });
 
-  it('$or).to work', function () {
+  it('$or should work', function () {
     var a = [0,1,2]
       , t1 = operators.$size(a, 2) // fail
       , t2 = operators.$in(1, a) // pass
@@ -84,7 +84,7 @@ describe('expect(operators', function(){
     expect(operators.$or([ t1, t3 ])).to.be(false);
   });
 
-  it('$nor).to work', function () {
+  it('$nor should work', function () {
     var a = [0,1,2]
       , t1 = operators.$size(a, 2) // fail
       , t2 = operators.$in(1, a) // pass
@@ -93,7 +93,7 @@ describe('expect(operators', function(){
     expect(operators.$nor([ t1, t3 ])).to.be(true);
   });
 
-  it('$and).to work', function () {
+  it('$and should work', function () {
     var a = [0,1,2]
       , t1 = operators.$size(a, 3) // pass
       , t2 = operators.$in(1, a) // pass
