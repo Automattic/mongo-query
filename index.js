a465179bce57de89f47c0220718d878210cfb7e1
Guillermo Rauch <rauchg@gmail.com>
2012-09-19 13:51:20 -0700

    Added initial skeleton for query API

---
+++ index.js
@@ -0,0 +1,21 @@
+
+/**
+ * Module dependencies.
+ */
+
+var type = require('type')
+  , filter = require('./filter')
+
+/**
+ * Module exports.
+ */
+
+module.exports = query;
+
+/**
+ * Execute a query.
+ */
+
+function query(){
+
+}
