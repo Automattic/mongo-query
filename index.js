f5604a0481ffa17304b6809a9bfbd5183542fc62
Guillermo Rauch <rauchg@gmail.com>
2012-09-19 16:47:28 -0700

    index: added `query#get` and `query#set`

--- index.js
+++ index.js
@@ -3,14 +3,23 @@
  * Module dependencies.
  */
 
-var type = require('type')
-  , filter = require('./filter')
+var filter = require('./filter')
+  , type;
+
+// dual `type` dependency
+try {
+  type = require('type');
+} catch(e){
+  type = require('type-component');
+};
 
 /**
  * Module exports.
  */
 
-module.exports = query;
+module.exports = exports = query;
+exports.get = get;
+exports.set = set;
 
 /**
  * Execute a query.
@@ -18,4 +27,29 @@ module.exports = query;
 
 function query(){
 
+}
+
+/**
+ * Gets the given key.
+ *
+ * @param {Object} object to query
+ * @param {String} key
+ * @api public
+ */
+
+function get(obj, key){
+  return filter.getPathValue(key, obj);
+}
+
+/**
+ * Sets the given key.
+ *
+ * @param {Object} object to query
+ * @param {String} key
+ * @param {Object} value
+ * @api public
+ */
+
+function set(obj, key, val){
+  return filter.setPathValue(key, obj, val);
 }
