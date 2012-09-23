780fb363d0b2442b5352d1c80c3da205cd4e1d20
Guillermo Rauch <rauchg@gmail.com>
2012-09-23 12:37:32 -0700

    mods: added unset transaction

--- mods.js
+++ mods.js
@@ -32,3 +32,25 @@ exports.$set = function $set(obj, key, val){
       throw new Error('$set only supports object not ' + type(obj));
   }
 };
+
+/**
+ * Performs an `$unset`
+ *
+ * @param {Object} object to modify
+ * @param {String} key to alter
+ * @param {String} value to set
+ * @return {Function} transaction
+ */
+
+exports.$unset = function $unset(obj, key){
+  switch (type(obj)) {
+    case 'array':
+    case 'object':
+      if (obj.hasOwnProperty(key)) {
+        return function(){
+          // reminder: `delete arr[1]` === `delete arr['1']` [!]
+          delete obj[key];
+        };
+      }
+  }
+};
