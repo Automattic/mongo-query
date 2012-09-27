0fb2f5064c4d51a4896f0eceedf65d37c37fddd2
Guillermo Rauch <rauchg@gmail.com>
2012-09-27 11:36:41 -0700

    test: removed helper tests - were lame

--- test/query.js
+++ test/query.js
@@ -4,35 +4,6 @@ var expect = require('expect.js');
 
 describe('query', function(){
 
-  describe('helpers', function(){
-    it('should expose #get', function(){
-      expect(query.get({ a: { b: 'c' } }, 'a.b')).to.be('c');
-    });
-
-    it('should expose #set', function(){
-      var obj = { a: 'b', c: { d: 'e' } };
-      expect(query.get(obj, 'c.f')).to.be(undefined);
-      query.set(obj, 'c.f', 'g');
-      expect(query.get(obj, 'c.f')).to.be('g');
-    });
-
-    it('should expose filtering api', function(){
-      var flt = query.filter({ 'c.f': 'g' });
-      expect(flt.test({ e: { d: '' } })).to.eql([]);
-      expect(flt.test({ c: { f: '' } })).to.eql([]);
-      expect(flt.test([{ c: { f: 'g' } }])).to.eql([{ c: { f: 'g' } }]);
-      expect(flt.test({ c: { f: 'g' } }, {
-        type: 'single'
-      })).to.eql(true);
-    });
-
-    it('should produce a empty changeset upon no match', function(){
-      expect(query({}, {}, {})).to.eql([]);
-      expect(query({ a: 'b' }, {}, {})).to.eql([]);
-      expect(query({ a: 'b' }, { c: 'd' }, {})).to.eql([]);
-    });
-  });
-
   describe('$set', function(){
     it('should $set simple key', function(){
       var obj = { a: 'b' };
