b193a82c66c904b4259d06b97a196dbdc1c60035
Guillermo Rauch <rauchg@gmail.com>
2012-09-29 12:16:32 -0700

    test: added subdoc filtering tests

--- test/filter.js
+++ test/filter.js
@@ -73,4 +73,87 @@ describe('filter', function(){
     });
   });
 
+  describe('arrays', function(){
+    it('simple', function(){
+      var ret = filter({
+        a: {
+          b: [1, 2, 3, 1]
+        }
+      }, { 'a.b': 1 });
+
+      expect(ret).to.eql({ 'a.b': [1, 1] });
+    });
+
+    it('subdocuments', function(){
+      var ret = filter({
+        a: {
+          b: {
+            c: [
+              { d: 5, hello: 'world' },
+              { d: 3, hello: 'world 2' },
+              { d: 5, tobi: 'ferret' },
+            ]
+          }
+        }
+      }, { 'a.b.c.d': 5 });
+
+      expect(ret).to.eql({ 'a.b.c.d': [
+        { d: 5, hello: 'world' },
+        { d: 5, tobi: 'ferret' }
+      ] });
+    });
+
+    it('query operators', function(){
+      var ret = filter({
+        ferrets: [
+          { name: 'tobi' },
+          { name: 'loki' },
+          { name: 'jane', likes: ['food'] }
+        ]
+      }, { 'ferrets.likes': { $exists: true } });
+
+      expect(ret).to.eql({
+        ferrets: [{ name: 'jane', likes: ['food'] }]
+      });
+    });
+
+    it('query subset narrow down', function(){
+      var ret = filter({
+        ferrets: [
+          { name: 'tobi', age: 5 },
+          { name: 'tobo', age: 4 },
+          { name: 'tomas', age: 10 }
+        ]
+      }, { 'ferrets.name': /^t/, 'ferrets.age': { $lt: 8 } });
+
+      expect(ret).to.eql({
+        ferrets: [{ name: 'tobi', age: 5 }, { name: 'tobo', age: 4 }]
+      });
+    });
+
+    it('multiple matches', function(){
+      var ret = filter({
+        ferrets: [
+          { name: 'tobi', age: 5 },
+          { name: 'loki', age: 3 },
+          { name: 'jane', age: 8 }
+        ],
+        people: [
+          { name: 'tj', programmer: true },
+          { name: 'marco', programmer: true },
+          { name: 'thianh', programmer: false },
+          { name: 'meredith', programmer: false }
+        ]
+      }, { 'ferrets.age': { $gte: 5 }, 'people.programmer': true });
+
+      expect(ret).to.eql({
+        ferrets: [{ name: 'jane' }],
+        people: [
+          { name: 'tj', programmer: true },
+          { name: 'marco', programmer: true }
+        ]
+      });
+    });
+  });
+
 });
