6b8744e2e3c4645cf2c9ad3bdf6300d654d6360a
Guillermo Rauch <rauchg@gmail.com>
2012-09-29 11:37:12 -0700

    mods: leverage `eql`

--- mods.js
+++ mods.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var debug = require('debug')('mongo-query')
+var eql = require('./eql')
+  , debug = require('debug')('mongo-query')
   , type, keys, dot;
 
 try {
@@ -457,58 +458,6 @@ function pull(vals){
     }
     return true;
   };
-}
-
-/**
- * Compares an existing value with a supplied matcher.
- *
- * @param {Object} matcher
- * @param {Object} value
- * @return {Boolean} true if they match
- */
-
-function eql(matcher, val){
-  switch (type(matcher)) {
-    case 'null':
-    case 'undefined':
-      // we treat null as undefined
-      return null == val;
-
-    case 'array':
-      if ('array' == type(val) && matcher.length == val.length) {
-        for (var i = 0; i < matcher.length; i++) {
-          if (!eql(val[i], matcher[i])) return false;
-        }
-        return true;
-      } else {
-        return false;
-      }
-      break;
-
-    case 'object':
-      // object can match keys in any order
-      var keys = {};
-
-      // we match all values of `matcher` in `val`
-      for (var i in matcher) {
-        if (matcher.hasOwnProperty(i)) {
-          if (!eql(matcher[i], val[i])) return false;
-        }
-        keys[i] = true;
-      }
-
-      // we make sure `val` doesn't have extra keys
-      for (var i in val) {
-        if (val.hasOwnProperty(i) && !keys.hasOwnProperty(i)) {
-          return false;
-        }
-      }
-
-      return true;
-
-    default:
-      return matcher === val;
-  }
 }
 
 /**
