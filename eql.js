1c461da7d0311d48134081f94d73fb8b20df804c
Guillermo Rauch <rauchg@gmail.com>
2012-09-29 11:36:50 -0700

    eql: abstracted out into its own file.

---
+++ eql.js
@@ -0,0 +1,73 @@
+
+/**
+ * Module dependencies.
+ */
+
+var type;
+
+try {
+  type = require('type');
+} catch(e){
+  type = require('type-component');
+}
+
+/**
+ * Module exports.
+ */
+
+module.exports = eql;
+
+/**
+ * MongoDB style value comparisons.
+ *
+ * @param {Object} matcher
+ * @param {Object} value
+ * @return {Boolean} true if they match
+ */
+
+function eql(matcher, val){
+  switch (type(matcher)) {
+    case 'null':
+    case 'undefined':
+      // we treat null as undefined
+      return null == val;
+
+    case 'regexp':
+      return matcher.test(val);
+
+    case 'array':
+      if ('array' == type(val) && matcher.length == val.length) {
+        for (var i = 0; i < matcher.length; i++) {
+          if (!eql(val[i], matcher[i])) return false;
+        }
+        return true;
+      } else {
+        return false;
+      }
+      break;
+
+    case 'object':
+      // object can match keys in any order
+      var keys = {};
+
+      // we match all values of `matcher` in `val`
+      for (var i in matcher) {
+        if (matcher.hasOwnProperty(i)) {
+          if (!eql(matcher[i], val[i])) return false;
+        }
+        keys[i] = true;
+      }
+
+      // we make sure `val` doesn't have extra keys
+      for (var i in val) {
+        if (val.hasOwnProperty(i) && !keys.hasOwnProperty(i)) {
+          return false;
+        }
+      }
+
+      return true;
+
+    default:
+      return matcher === val;
+  }
+}
