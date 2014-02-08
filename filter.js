a87754f5f0268d6a83044018e34e7b37e1d91ac3
Greg Terrono <terronogr@gmail.com>
2014-02-08 13:51:48 -0800

    Switched var style in filter.js

--- filter.js
+++ filter.js
@@ -6,11 +6,9 @@
 var eql = require('mongo-eql');
 var ops = require('./ops');
 var debug = require('debug')('mongo-query');
-var object, type, dot;
-
-dot = require('dot-component');
-type = require('type-component');
-object = require('object-component');
+var dot = require('dot-component');
+var type = require('type-component');
+var object = require('object-component');
 
 /**
  * Module exports.
